53971a9df20db65c076e0023daee8d6e71354044
Francisco Fernandez <pacofer71@gmail.com>
2021-05-06 09:10:56 +0200

    first commit

---
+++ src/paquete1/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package paquete1;
+
+/**
+ * @author pacof
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+	}
+
+}
